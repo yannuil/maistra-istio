maistra/istio

Introduce GatewayControllerMode

pilot/pkg/serviceregistry/kube/controller/controller.go
@@ -372,6 +372,11 @@ func NewController(kubeClient kubelib.Client, options Options) *Controller {
 			c.registerHandlers(nsInformer, "Namespaces", c.onSystemNamespaceEvent, nil)
 		}
 
+		// don't use discoveryFilters if in gw-controller mode. always watch all namespaces
+		if features.EnableGatewayControllerMode {
+			c.opts.DiscoveryNamespacesFilter = filter.NewDiscoveryNamespacesFilter(c.nsLister, []*metav1.LabelSelector{})
+		}
+
 		if c.opts.DiscoveryNamespacesFilter == nil {
 			c.opts.DiscoveryNamespacesFilter = filter.NewDiscoveryNamespacesFilter(c.nsLister, options.MeshWatcher.Mesh().DiscoverySelectors)
 		}

yannuil/maistra-istio

Co-authored-by: Yann Liu <yannliu@redhat.com>
Co-authored-by: Daniel Grimm <dgrimm@redhat.com>
Signed-off-by: Yann Liu <yannliu@redhat.com>

pilot/pkg/serviceregistry/kube/controller/controller.go
@@ -338,6 +338,11 @@ func NewController(kubeClient kubelib.Client, options Options) *Controller {
 				nil,
 			)
 		}
+
+		// don't use discoveryFilters if in gw-controller mode. always watch all namespaces
+		if features.EnableGatewayControllerMode {
+			c.opts.DiscoveryNamespacesFilter = namespace.NewDiscoveryNamespacesFilter(c.namespaces, []*metav1.LabelSelector{})
+		}
 		if c.opts.DiscoveryNamespacesFilter == nil {
 			c.opts.DiscoveryNamespacesFilter = namespace.NewDiscoveryNamespacesFilter(c.namespaces, options.MeshWatcher.Mesh().DiscoverySelectors)
 		}